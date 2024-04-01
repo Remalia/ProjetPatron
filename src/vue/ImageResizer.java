08969f520e13276b90c1b8d2e18b2f3adb765d2d
Typhaine <22001015@etu.unicaen.fr>
2024-04-01 12:47:49 +0200

    javadoc de src/vue/*

--- src/vue/ImageResizer.java
+++ src/vue/ImageResizer.java
@@ -7,17 +7,37 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Classe qui permet de redimensionner les images
+ */
 public class ImageResizer {
 
+    /***
+     * Permet d'obtenir la bonne taille d'une image pour la barre de navigation
+     * @param pathImg : chemin de l'image
+     * @return l'image redimensionnée
+     * @throws IOException Exception si l'image n'est pas trouvée
+     */
     public static Image getGoodImageSizeNavBar(String pathImg) throws IOException {
         return ImageIO.read(new File(pathImg)).getScaledInstance(50,50,java.awt.Image.SCALE_SMOOTH);
     }
 
+    /***
+     * Permet d'obtenir la bonne taille d'une image pour la checkbox (fullscreen)
+     * @return l'image redimensionnée
+     * @throws IOException Exception si l'image n'est pas trouvée
+     */
     public static Image getGoodImageCheckBox() throws IOException {
         String pathImg = Param.isFullScreen() ? "assets/images/Menu/case_cochee.png" : "assets/images/Menu/case.png";
         return ImageIO.read(new File(pathImg)).getScaledInstance(50,50,java.awt.Image.SCALE_SMOOTH);
     }
 
+    /***
+     * Permet d'obtenir la bonne taille d'une image pour le menu de sélection de niveau
+     * @param pathImg : chemin de l'image
+     * @return l'image redimensionnée
+     * @throws IOException Exception si l'image n'est pas trouvée
+     */
     public static Image getGoodImageSizeSelectionNiveau(String pathImg) throws  IOException {
         double widthMult = 0.10;
         double heightMult = 0.12;
@@ -30,6 +50,12 @@ public class ImageResizer {
         return ImageIO.read(new File(pathImg)).getScaledInstance((int)(width*widthMult),(int)(height*heightMult), Image.SCALE_SMOOTH);
     }
 
+    /***
+     * Permet d'obtenir la bonne taille d'une image pour le menu principal
+     * @param pathImg : chemin de l'image
+     * @return l'image redimensionnée
+     * @throws IOException Exception si l'image n'est pas trouvée
+     */
     public static Image getGoodImageSizeMenuPrincipal(String pathImg) throws IOException {
         double widthMult = 0.25;
         double heightMult = 0.125;
