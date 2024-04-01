8fb5a983d2ce4a52ac792a326688e48b35ab1d21
Typhaine <22001015@etu.unicaen.fr>
2024-04-01 13:04:23 +0200

    javadoc de src/model/*

--- src/model/Parser.java
+++ src/model/Parser.java
@@ -7,9 +7,14 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.List;
 
+/***
+ * Classe qui permet de parser un fichier
+ */
 public class Parser {
-    /**
-     * @param file Fichier à lire
+
+    /***
+     * Permet de récupérer toutes les balises et les valeurs associées dans un fichier
+     * @param file : Fichier à lire
      * @return une liste HashMap clé/valeur avec toutes les balises et les valeurs associées sous forme de string
      * @throws FileNotFoundException Fichier Introuvable
      */
@@ -34,7 +39,7 @@ public class Parser {
 
     /**
      * Supprime les espaces en début de chaine de caractère
-     * @param chaine la chaine en question
+     * @param chaine : la chaine en question
      * @return la chaine sans les espaces
      */
     private static String deleteStartSpace(String chaine) {
--- src/model/Score.java
+++ src/model/Score.java
@@ -4,6 +4,9 @@ import ProjetPatron.src.vue.Menu.GamePane;
 
 import java.awt.*;
 
+/***
+ * Classe qui permet de gérer le score de la partie
+ */
 public class Score {
     private static Score instance;
     private int id;
@@ -11,6 +14,10 @@ public class Score {
     private float scoreAct;
     private float  scoreAffiche ;
 
+    /***
+     * Constructeur de la classe Score
+     * @param id : id du score
+     */
     private Score(int id){
 
         this.id = id;
@@ -21,6 +28,10 @@ public class Score {
         this.scoreAffiche = 0;
     }
 
+    /***
+     * Permet de récupérer le score maximum
+     * @return le score max de la partie colorée
+     */
     public float getScoreMax(){
         float size = GamePane.getInstance().getWidth() * GamePane.getInstance().getHeight();
         float red = 0;
@@ -35,6 +46,10 @@ public class Score {
         return size - red;
     }
 
+    /***
+     * Permet de récupérer le score actuel
+     * @return le score actuel de la partie colorée
+     */
     public float getScoreAct(){
         int size = GamePane.getInstance().getWidth() * GamePane.getInstance().getHeight();
         int green = 0;
@@ -50,7 +65,10 @@ public class Score {
     }
 
 
-
+    /***
+     * Permet de calculer le score
+     * @return le score actuel
+     */
     public float calculScore(){
         this.scoreAct = this.getScoreAct();
         if(scoreAct != 0){
@@ -60,6 +78,11 @@ public class Score {
         return this.scoreAffiche;
     }
 
+    /***
+     * Permet de récupérer l'instance unique du score
+     * @param id : id du score
+     * @return l'instance du score
+     */
     public static Score getInstance(int id){
         if (instance == null || instance.getId() != id){
             instance = new Score(id);
@@ -67,6 +90,10 @@ public class Score {
         return instance;
     }
 
+    /***
+     * Permet de récupérer l'id du score
+     * @return l'id du score
+     */
     public int getId(){
         return id;
     }
