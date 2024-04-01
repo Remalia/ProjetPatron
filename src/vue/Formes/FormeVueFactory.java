b602541988cee7ac218993a9b5b4fc18284b5903
Typhaine <22001015@etu.unicaen.fr>
2024-04-01 12:28:01 +0200

    javadoc de src/vue/Formes/*

--- src/vue/Formes/FormeVueFactory.java
+++ src/vue/Formes/FormeVueFactory.java
@@ -1,7 +1,13 @@
 package ProjetPatron.src.vue.Formes;
 
+/***
+ * Interface qui permet de créer une vue de forme
+ */
 public interface FormeVueFactory {
 
+    /***
+     * Permet de créer la vue d'une forme
+     */
     FormeVue createFormeVue();
 
 }
--- src/vue/Formes/TriangleVue.java
+++ src/vue/Formes/TriangleVue.java
@@ -5,14 +5,26 @@ import ProjetPatron.src.model.Formes.Forme;
 
 import java.awt.*;
 
+/***
+ * Classe qui permet de gérer l'affichage d'un triangle
+ 
+ */
 public class TriangleVue extends FormeVue{
 
     private Polygon polygon;
 
+    /***
+     * Constructeur de la vue d'un triangle
+     * @param forme : forme à afficher
+     */
     public TriangleVue(Forme forme) {
         super(forme);
     }
 
+    /***
+     * Permet de dessiner un triangle
+     * @param g : graphics
+     */
     @Override
     public void drawForme(Graphics g) {
         g.setColor(getForme().getColor());
@@ -21,6 +33,11 @@ public class TriangleVue extends FormeVue{
         g.fillPolygon(polygon);
     }
 
+    /***
+     * Permet de savoir si un point est dans le triangle
+     * @param coord : coordonnées du point
+     * @return vrai si le point est dans le triangle, faux sinon
+     */
     @Override
     public boolean isInForme(Coord coord) {
         return polygon.contains(new Point(coord.getX(),coord.getY()));
